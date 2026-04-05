c4267d34cd0bbe5cb99a1ef1427b779855267330
mitumial <ace.vortex76@gmail.com>
2026-04-05 13:22:47 -0500

    add mapper

--- MS_Finanzas/src/main/java/com/example/demo/infra/persistence/entity/CategoryEntity.java
+++ MS_Finanzas/src/main/java/com/example/demo/infra/persistence/entity/CategoryEntity.java
@@ -12,19 +12,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
 @Table(name = "categorias")
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 public class CategoryEntity {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
