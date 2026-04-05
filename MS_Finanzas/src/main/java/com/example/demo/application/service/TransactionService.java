7de36c940f0f7deb521025b0da14a21f5be54106
mitumial <ace.vortex76@gmail.com>
2026-04-05 13:26:16 -0500

    add repository port to service

--- MS_Finanzas/src/main/java/com/example/demo/application/service/TransactionService.java
+++ MS_Finanzas/src/main/java/com/example/demo/application/service/TransactionService.java
@@ -2,15 +2,22 @@ package com.example.demo.application.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.application.repository.TransactionRepositoryPort;
 import com.example.demo.application.usecase.CreateTransactionUseCase;
 import com.example.demo.domain.model.Transaction;
 
 @Service
 public class TransactionService implements CreateTransactionUseCase{
 
+    private final TransactionRepositoryPort transactionRepositoryPort;
+
+    public TransactionService(TransactionRepositoryPort transactionRepositoryPort) {
+        this.transactionRepositoryPort = transactionRepositoryPort;
+    }
+
     @Override
     public Transaction createTransaction(Transaction transaction) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return transactionRepositoryPort.save(transaction);
     }
 
 }
