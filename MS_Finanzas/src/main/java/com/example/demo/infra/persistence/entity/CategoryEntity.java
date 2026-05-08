b4799044a4f6b19fb049fcae8e67fd700fb9cfc5
Junior <junimore110@gmail.com>
2026-05-08 18:03:32 -0500

    Correction categories

--- MS_Finanzas/src/main/java/com/example/demo/infra/persistence/entity/CategoryEntity.java
+++ MS_Finanzas/src/main/java/com/example/demo/infra/persistence/entity/CategoryEntity.java
@@ -3,6 +3,7 @@ package com.example.demo.infra.persistence.entity;
 import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -20,17 +21,22 @@ import lombok.Data;
 @Table(name = "categorias")
 @Data
 public class CategoryEntity {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID categoriaId;
 
-    @Column(name="nombre", nullable=false, unique=true)
+    @Column(name = "nombre", nullable = false, unique = true)
     private String nombre;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name="titular_id")
+    @JoinColumn(name = "titular_id")
     private TitularEntity titular;
 
-    @OneToMany(mappedBy="categoria", cascade={CascadeType.MERGE, CascadeType.PERSIST})
+    @OneToMany(mappedBy = "categoria", cascade = {
+            CascadeType.MERGE,
+            CascadeType.PERSIST
+    })
+    @JsonIgnore
     private List<TransactionEntity> transacciones;
 }
