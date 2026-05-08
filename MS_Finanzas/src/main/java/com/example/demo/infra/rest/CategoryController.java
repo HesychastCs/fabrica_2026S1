96313f5925141dbb6d90fd5e0dc68824887d9131
Junior <junimore110@gmail.com>
2026-05-08 13:37:36 -0500

    Actualiza CategoryController

--- MS_Finanzas/src/main/java/com/example/demo/infra/rest/CategoryController.java
+++ MS_Finanzas/src/main/java/com/example/demo/infra/rest/CategoryController.java
@@ -5,14 +5,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.application.service.CategoryService;
 import com.example.demo.domain.model.Category;
@@ -25,6 +18,7 @@ import jakarta.validation.Valid;
 
 
 
+@CrossOrigin(origins = "https://front-end-fe20261.vercel.app")
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
