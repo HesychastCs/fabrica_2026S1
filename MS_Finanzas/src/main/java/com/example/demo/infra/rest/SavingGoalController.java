5e2cf8f0053dbd45f42672d65e37de6a6b132d5e
Junior <junimore110@gmail.com>
2026-05-09 00:02:50 -0500

    Nueva cofig de Cors

--- MS_Finanzas/src/main/java/com/example/demo/infra/rest/SavingGoalController.java
+++ MS_Finanzas/src/main/java/com/example/demo/infra/rest/SavingGoalController.java
@@ -16,7 +16,20 @@ import com.example.demo.infra.rest.dto.SavingGoalResponse;
 
 import jakarta.validation.Valid;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(
+        origins = {
+                "https://front-end-fe20261.vercel.app",
+                "https://front-end-fe20261-c4otfrley-junior-morenos-projects.vercel.app"
+        },
+        allowedHeaders = "*",
+        methods = {
+                RequestMethod.GET,
+                RequestMethod.POST,
+                RequestMethod.PUT,
+                RequestMethod.DELETE,
+                RequestMethod.OPTIONS
+        }
+)
 @RestController
 @RequestMapping("/api/saving-goals")
 public class SavingGoalController {
